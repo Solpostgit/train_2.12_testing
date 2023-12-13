3fd774e0e6540c93d9005efc09af9913812d35ff
Solpostgit <begemot_off@mail.ru>
2023-12-13 20:24:17 +0300

    add Tests

--- src/test/java/org/example/UniversityTest.java
+++ src/test/java/org/example/UniversityTest.java
@@ -1,25 +1,36 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class UniversityTest {
 
-    @Test
-    public void getAllStudents() {
-        Student student1 = new Student("Евгений", 40, true);
-        Student student2 = new Student("Марина", 33, false);
-        Student student3 = new Student("Алина", 35, false);
+    private Student student1;
+    private Student student2;
+    private Student student3;
+    private University university;
+
+    @BeforeEach
+    public void setUp() {
+        student1 = new Student("Евгений", 40, true);
+        student2 = new Student("Марина", 33, false);
+        student3 = new Student("Алина", 35, false);
 
         University university = new University();
 
         university.addStudent(student1);
         university.addStudent(student2);
         university.addStudent(student3);
+    }
+
+    @Test
+    public void getAllStudents() {
 
         List<Student> expected = university.getAllStudents();
 
@@ -28,6 +39,26 @@ public class UniversityTest {
         actual.add(student1);
         actual.add(student2);
         actual.add(student3);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getAllStudentsNotNull() {
+        University university = new University();
+        List<Student> expected = university.getAllStudents();
+
+        assertNotNull(expected);
+    }
+
+    @Test
+    public void getAllStudentsIsMale() {
+
+        List<Student> expected = university.getAllStudents(true);
+
+        List<Student> actual = new ArrayList<Student>();
+
+        actual.add(student1);
 
         assertEquals(expected, actual);
     }
